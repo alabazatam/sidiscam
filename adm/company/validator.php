5fef9403c207eb1aa01894a78402f16f3fb9472f
marcos <marcos@marcos-desktop>
2016-06-21 20:31:34 -0430

    validacion de numeros telefonicos

--- adm/company/validator.php
+++ adm/company/validator.php
@@ -56,7 +56,7 @@
 		$validator_values['phone1'] = array(
 			
 			"minlength" => 10,
-			"maxlength" => 11,
+			"maxlength" => 15,
 			"type" => "number",
 			"label" => "Teléfono Principal",
 			"required" => true
@@ -64,7 +64,7 @@
 		$validator_values['phone2'] = array(
 			
 			"minlength" => 10,
-			"maxlength" => 11,
+			"maxlength" => 15,
 			"type" => "text",
 			"label" => "Teléfono Secundario",
 			"required" => false
